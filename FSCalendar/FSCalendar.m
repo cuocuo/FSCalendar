1deed1a1aa0bb816fcd1ff1f2524fbe56ec8f73a
shiheng <shiheng@nicomama.com>
2018-04-10 17:29:22 +0800

    调整UI

--- FSCalendar/FSCalendar.m
+++ FSCalendar/FSCalendar.m
@@ -337,8 +337,8 @@ typedef NS_ENUM(NSUInteger, FSCalendarOrientation) {
         
         self.calendarHeaderView.frame = CGRectMake(0, 0, self.fs_width, headerHeight);
         self.calendarWeekdayView.frame = CGRectMake(_collectionViewLayout.sectionInsets.left, self.calendarHeaderView.fs_bottom, self.contentView.fs_width - (_collectionViewLayout.sectionInsets.left + _collectionViewLayout.sectionInsets.right), weekdayHeight);
-        _prePageButton.frame = CGRectMake((self.fs_width - 144)/2, headerHeight*0.1, 24, headerHeight*0.9);
-        _nextPageButton.frame = CGRectMake(self.fs_width - (self.fs_width - 144)/2 - 24, headerHeight*0.1, 24, headerHeight*0.9);
+        _prePageButton.frame = CGRectMake((self.fs_width - 144)/2, headerHeight*0.1, 30, headerHeight*0.9);
+        _nextPageButton.frame = CGRectMake(self.fs_width - (self.fs_width - 144)/2 - 30, headerHeight*0.1, 30, headerHeight*0.9);
         _deliver.frame = CGRectMake(self.calendarHeaderView.fs_left, self.calendarHeaderView.fs_top, self.calendarHeaderView.fs_width, headerHeight+weekdayHeight);
         _deliver.hidden = self.calendarHeaderView.hidden;
         if (!self.floatingMode) {
